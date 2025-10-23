328c12b0e571be2e4be36c764d07458131802240
Maxim <maxim.paw2010@yandex.ru>
2025-10-23 18:46:34 +0300

    логика создания и присваивания корзины юзеру с ее функциональностью

--- shared/components/shared/index.ts
+++ shared/components/shared/index.ts
@@ -15,4 +15,6 @@ export { SearchInput } from "./search-input";
 export { ProductImage } from "./product-image";
 export { ChooseProductForm } from "./choose-product-form";
 export { ChoosePizzaForm } from "./choose-pizza-form";
+export { CartButton } from "./cart-button";
+export { CartDrawer } from "./cart-drawer";
 export * from "./modals";
